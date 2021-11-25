006176c433593e267c50dff4ad4ab3921c9f049e
gamepubcorp <gamepub.system@gmail.com>
2021-11-25 18:57:34 +0900

    [update] deprecated script delete

--- Assets/GamePubSDK/Demo/Script/UIPanel/Setting_Panel.cs
+++ Assets/GamePubSDK/Demo/Script/UIPanel/Setting_Panel.cs
@@ -4,25 +4,14 @@ using UnityEngine;
 using GamePub.PubSDK;
 
 public class Setting_Panel : MonoBehaviour
-{
-    //public GameObject pushToggle;
-    //public GameObject adPushToggle;
-    //public GameObject nightPushToggle;
-
+{    
     public ToggleController pushToggle;
     public ToggleController adPushToggle;
     public ToggleController nightPushToggle;
 
     // Start is called before the first frame update
     public void Start()
-    {
-        //pushToggle.GetComponent<SliderToggle>().SetToggle(
-        //    UserInfoManager.Ins.loginResult.UserLoginInfo.AgreePush);
-        //adPushToggle.GetComponent<SliderToggle>().SetToggle(
-        //    UserInfoManager.Ins.loginResult.UserLoginInfo.AgreeAd);
-        //nightPushToggle.GetComponent<SliderToggle>().SetToggle(
-        //    UserInfoManager.Ins.loginResult.UserLoginInfo.AgreeNight);
-
+    {        
         pushToggle.isOn = UserInfoManager.Ins.loginResult.UserLoginInfo.AgreePush;
         adPushToggle.isOn = UserInfoManager.Ins.loginResult.UserLoginInfo.AgreeAd;
         nightPushToggle.isOn = UserInfoManager.Ins.loginResult.UserLoginInfo.AgreeNight;
@@ -30,32 +19,6 @@ public class Setting_Panel : MonoBehaviour
 
     public void OnPushClick(bool enabled)
     {
-        //SliderToggle push = pushToggle.GetComponent<SliderToggle>();
-        //push.callback = (bool status) =>
-        //{
-        //    GamePubSDK.Ins.UserInfoUpdate(
-        //    UserInfoManager.Ins.currentCode,
-        //    status,
-        //    UserInfoManager.Ins.pushNight,
-        //    UserInfoManager.Ins.pushAd,
-        //    result =>
-        //    {
-        //        result.Match(
-        //            value =>
-        //            {
-
-        //                UserInfoManager.Ins.push = value.AgreePush;
-        //            },
-        //            error =>
-        //            {
-
-        //            });
-        //    });
-        //};
-        //push.ChangeToggle();
-
-        Debug.Log("OnPushClick : " + enabled);
-
         GamePubSDK.Ins.UserInfoUpdate(
             UserInfoManager.Ins.currentCode,
             enabled,
@@ -77,30 +40,6 @@ public class Setting_Panel : MonoBehaviour
 
     public void OnAdPushClick(bool enabled)
     {
-        //SliderToggle push = adPushToggle.GetComponent<SliderToggle>();
-        //push.callback = (bool status) =>
-        //{
-        //    GamePubSDK.Ins.UserInfoUpdate(
-        //    UserInfoManager.Ins.currentCode,
-        //    UserInfoManager.Ins.push,
-        //    UserInfoManager.Ins.pushNight,
-        //    status,
-        //    result =>
-        //    {
-        //        result.Match(
-        //            value =>
-        //            {                        
-        //                UserInfoManager.Ins.pushAd = value.AgreeAd;
-        //            },
-        //            error =>
-        //            {
-        //            });
-        //    });
-        //};
-        //push.ChangeToggle();
-
-        Debug.Log("OnAdPushClick : " + enabled);
-
         GamePubSDK.Ins.UserInfoUpdate(
             UserInfoManager.Ins.currentCode,
             UserInfoManager.Ins.push,
@@ -122,30 +61,6 @@ public class Setting_Panel : MonoBehaviour
 
     public void OnNightPushClick(bool enabled)
     {
-        //SliderToggle push = nightPushToggle.GetComponent<SliderToggle>();
-        //push.callback = (bool status) =>
-        //{
-        //    GamePubSDK.Ins.UserInfoUpdate(
-        //    UserInfoManager.Ins.currentCode,
-        //    UserInfoManager.Ins.push,
-        //    status,
-        //    UserInfoManager.Ins.pushAd,
-        //    result =>
-        //    {
-        //        result.Match(
-        //            value =>
-        //            {                        
-        //                UserInfoManager.Ins.pushNight = value.AgreeNight;
-        //            },
-        //            error =>
-        //            {                        
-        //            });
-        //    });
-        //};
-        //push.ChangeToggle();
-
-        Debug.Log("OnNightPushClick : " + enabled);
-
         GamePubSDK.Ins.UserInfoUpdate(
             UserInfoManager.Ins.currentCode,
             UserInfoManager.Ins.push,
